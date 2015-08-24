c243503966d945f86d6689270bc8aab6d849d9aa
ma2snct <knightmarespark@gmail.com>
2015-08-24 13:45:31 +0900

    csvモジュール使えた

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var csv = require('csv');
+var fs = require('fs');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('th a resource');
+	//res.send('data input');
+	csv()
+		.from.stream(fs.createReadStream(__dirname + '/../public/data.csv'))
+		.to.path(__dirname + '/../public/out.csv')
+		.on('record', function(row, index){
+			console.log('done')
+		});
 });
 
 module.exports = router;
