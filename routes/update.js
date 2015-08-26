57a9d562eb663353a477df0370ac6bf6fab96ba3
ma2snct <knightmarespark@gmail.com>
2015-08-26 14:09:55 +0900

    ゴリ押しだけどドキュメント更新できた

--- routes/update.js
+++ routes/update.js
@@ -9,50 +9,41 @@ var db = require('nano')('http://localhost:5984/sotsu');
 var sotsu = nano.use('sotsu');
 var assert = require('assert');
 
+var rows = [];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	//res.send('data input');
-	//DBに書き込む内容をためこむやーつ
-	var rows = [];
-	var rev;
-	var id;
 
 	db.get('Aug24th-2', function(err, body){
 		if(!err){
-			//console.log(body);
-			//console.log(body._rev);
-			//console.log(body._id);
-			id = body._id;
-			rev = "'"+body._rev+"'";
-			console.log(id);
-			console.log(rev);
+			csv()
+				.from.stream(fs.createReadStream(__dirname + '/../public/data.txt'))
+				//.to.path(__dirname + '/../public/out.csv')
+
+				//行数だけループするっぽい
+				.on('record', function(row, index){
+					rows.push(row);
+				})
+				.on('end', function(count){
+					console.log('text input is done');
+					
+					sotsu.insert({_id:'Aug24th-2', _rev:body._rev, data_txt:rows}, function(err, body, header){
+						if(err){
+							console.log('[sotsu.insert]', err.message);
+							return;
+						}else{
+							console.log('you have changed the Aug24th-2.');
+							console.log(body);
+						}
+					});	
+				});
 		}else{
 			console.log('it has err');
 			console.log(err);
 		}
 	});
-	
-	csv()
-		.from.stream(fs.createReadStream(__dirname + '/../public/data.txt'))
-		//.to.path(__dirname + '/../public/out.csv')
-
-		//行数だけループするっぽい
-		.on('record', function(row, index){
-			rows.push(row);
-		})
-		.on('end', function(count){
-			console.log('done');
-			
-			sotsu.insert({_id:'Aug24th-2', _rev:'2-8735279bea13b8b26e62cd5e71e8d3b2', data_txt:'がっこうぐらし'},  function(err, body, header){
-				if(err){
-					console.log('[sotsu.insert]', err.message);
-					return;
-				}
-				console.log('you have changed the Aug24th-3.');
-				console.log(body);
-			});
-			
-		});
 });
+
+
 
 module.exports = router;
